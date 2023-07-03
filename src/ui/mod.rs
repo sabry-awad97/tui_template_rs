f593b992763da1fb673dc808bf8b44ae31bb9679
Sabry Awad <dr.sabry1997@gmail.com>
2023-07-03 10:33:42 +0300

    🎉 feat(ui/mod.rs): Add template UI rendering 🔧 refactor(ui/mod.rs): Update UI rendering logic and style
    In this commit, the UI rendering in the `render` function of `ui/mod.rs` was modified to display a template interface using the tui library.
    
    Changes made:
    - Updated imports to include necessary modules from the tui library.
    - Modified the `render` function to display a template UI using the `Paragraph` widget.
    - Added a `Block` with a title, border, and rounded border type to the `Paragraph` widget.
    - Applied styling to the `Paragraph` widget with a cyan foreground color and black background color.
    - Set the alignment of the `Paragraph` widget to the center.
    - Passed the frame size to the `frame.render_widget` function.
    
    This change introduces a basic template UI to serve as a starting point for further development.
    
    Signed-off-by: Sabry Awad <dr.sabry1997@gmail.com>

--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -1,5 +1,25 @@
-use tui::{backend::Backend, Frame};
+use tui::{
+    backend::Backend,
+    layout::Alignment,
+    style::{Color, Style},
+    widgets::{Block, BorderType, Borders, Paragraph},
+    Frame,
+};
 
-use crate::App;
+use crate::app::App;
 
-pub fn render<B: Backend>(app: &mut App, frame: &mut Frame<'_, B>) {}
+pub fn render<B: Backend>(_app: &mut App, frame: &mut Frame<'_, B>) {
+    frame.render_widget(
+        Paragraph::new("This is a tui template.")
+            .block(
+                Block::default()
+                    .title("Template")
+                    .title_alignment(Alignment::Center)
+                    .borders(Borders::ALL)
+                    .border_type(BorderType::Rounded),
+            )
+            .style(Style::default().fg(Color::Cyan).bg(Color::Black))
+            .alignment(Alignment::Center),
+        frame.size(),
+    )
+}
